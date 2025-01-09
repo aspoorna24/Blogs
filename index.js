e9b4b32b3ddb28c658a04c494213ead749dd6a32
aspoorna24 <aspoorna24@gmail.com>
2025-01-09 15:25:31 +0530

    Remove MVC and Route to fix Deploy

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
-
+const Blog = require('./model/blog')
 const morgan = require("morgan");
-const BlogRoutes = require('./routes/blogRoutes');
+
 
 const app = express();
 
@@ -36,7 +36,7 @@ app.use(morgan("dev"));
 
   
 app.get("/", (req, res) => {
-   res.send('Hi From Server')
+   res.redirect('/blogs')
 });
 
 
@@ -50,8 +50,58 @@ app.get("/about-us", (req, res) => {
 });
 
 
+
+app.get('/blogs', (req, res) =>{
+  Blog.find().sort({createdAt:-1})
+  .then((result)=>{
+     res.render('index',{title:"All BLogs", blogs:result})
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+});
+
+app.post('/blogs',(req,res) =>{
+  const blog = new Blog (req.body);
+  
+  blog.save()
+  .then((result)=>{
+    res.redirect('/');
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+
 //blogs
-app.use('/blogs', BlogRoutes);
+app.get("/blogs/create",(req,res)=>{
+  res.render("create", { title: "Create New Blog" });
+});
+
+app.get('/blogs/:id',(req,res) =>{
+  const id = req.params.id;
+  Blog.findById(id)
+  .then(result =>{
+    res.render('details',{blog: result, title: "Blog Details"} )
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+  
+app.delete('blogs/:id',(req,res) =>{
+  const id = req.params.id;
+  
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+
 
 //404
 app.use((req, res) => {
